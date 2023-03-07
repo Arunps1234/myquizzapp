3d6d79cc6962ecc990f26058b0fb049abe827a4d
Arunps <gowdaaru1999@gmail.com>
2023-03-07 19:27:33 +0530

    I have updated the data

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -1,6 +1,6 @@
 export const questions = [
     {
-      questionText: " In which year was Mysore renamed as Karnataka?",
+      questionText: " In which year was Mysore renamed Karnataka?",
       answers: [
         {
           answerText: "1972",
